64b5c76cce38179c5ff99dd1a4e04448ea86c7fa
David James <djames@spreedly.com>
2021-04-03 15:58:45 -0400

    test4

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,11 @@ set -o pipefail
 
 repository=$(mktemp)
 git rev-parse --git-dir 2> "$repository"
-git rev-list --all
+#git rev-list --all
+branch="$(echo $GITHUB_REF | cut -d/ -f3)"
 
-first_commit="$( git rev-list main..$GITHUB_REF | tail -n 1)"
-numbert_of_commits="$( git rev-list main..$GITHUB_REF | wc -l)"
+first_commit="$( git rev-list main..$branch | tail -n 1)"
+numbert_of_commits="$( git rev-list main..$branch | wc -l)"
 
 echo "First commit is: ${first_commit}"
 echo "Number of commits: ${number_of_commits}"
