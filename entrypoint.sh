b0d69fbd8b79208331b40382904e9f012ab7eedc
David James <djames@spreedly.com>
2021-04-03 15:45:52 -0400

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -o pipefail
 
 repository=$(mktemp)
 git rev-parse --git-dir 2> "$repository"
+git rev-list --all
 
 first_commit="$( git rev-list main..HEAD | tail -n 1)"
 numbert_of_commits="$( git rev-list main..HEAD | wc -l)"
