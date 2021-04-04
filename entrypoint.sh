559539e587c0999811f6a0e22d5c2f6860e60928
David James <djames@spreedly.com>
2021-04-04 01:21:27 -0400

    new approach

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,22 @@
 set -xe
 set -o pipefail
 
-repository=$(mktemp)
-git rev-parse --git-dir 2> "$repository"
-#git rev-list --all
-branch="$(echo $GITHUB_REF | cut -d/ -f3)"
+git fetch main:main
+git diff --name-only main
 
-base="HEAD^"
-git rev-parse "$base"
-git rev-parse "HEAD"
-git rev-parse "main"
-git diff --check --color "$base..HEAD"
-
-first_commit="$( git rev-list --no-merges main.. | tail -n 1 )"
-numbert_of_commits="$( git rev-list --no-merges --count main.. )"
-
-echo "First commit is: ${first_commit}"
-echo "Number of commits: ${number_of_commits}"
+#repository=$(mktemp)
+#git rev-parse --git-dir 2> "$repository"
+##git rev-list --all
+#branch="$(echo $GITHUB_REF | cut -d/ -f3)"
+#
+#base="HEAD^"
+#git rev-parse "$base"
+#git rev-parse "HEAD"
+#git rev-parse "main"
+#git diff --check --color "$base..HEAD"
+#
+#first_commit="$( git rev-list --no-merges main.. | tail -n 1 )"
+#numbert_of_commits="$( git rev-list --no-merges --count main.. )"
+#
+#echo "First commit is: ${first_commit}"
+#echo "Number of commits: ${number_of_commits}"
